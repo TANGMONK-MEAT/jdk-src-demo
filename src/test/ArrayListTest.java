2adbd247447a152b88259286af32be12bd13437c
tangsengrou01 <58246525+tangsengrou01@users.noreply.github.com>
2020-07-31 20:38:37 +0800

    添加 ArrayListTest 测试类

---
+++ src/test/ArrayListTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Description:
+ * 调试 ArrayList 源码
+ * @author zwl
+ * @version 1.0
+ * @date 2020/7/31 20:32
+ */
+public class ArrayListTest {
+
+    public static void main(String[] args) {
+
+        List<Object> list =  new ArrayList<Object>();
+        list.add("123");
+        System.out.println("【ArrayListTest】" + list);
+    }
+}
